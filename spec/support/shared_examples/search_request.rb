c6d829969eb6585eaa5d17600410d677ace7fdfd
Richard Doe <richard.doe@rwdit.net>
2015-07-22 10:03:20 +0100

    Fix search request spec for absent query param.

--- spec/support/shared_examples/search_request.rb
+++ spec/support/shared_examples/search_request.rb
@@ -1,12 +1,12 @@
-shared_examples "search request" do
+shared_examples 'search request' do
   before(:each) do
     stub_request(:get, %r{www.europeana.eu/api/v2/search.json}).
       to_return(body: '{"success":true}')
   end
 
-  it_behaves_like "API request"
+  it_behaves_like 'API request'
 
-  context "with API key" do
+  context 'with API key' do
     let(:api_key) { 'xyz' }
     let(:params) { {} }
 
@@ -14,26 +14,27 @@ shared_examples "search request" do
       Europeana::API.api_key = api_key
     end
 
-    it "sends a Search request to the API" do
+    it 'sends a Search request to the API' do
       subject
       expect(a_request(:get, %r{www.europeana.eu/api/v2/search.json})).
         to have_been_made.once
     end
 
-    context "without query" do
-      it "sets an empty query" do
+    context 'without query' do
+      it 'sends without query' do
         subject
-        expect(a_request(:get, %r{www.europeana.eu/api/v2/search.json?query=})).
+        expect(a_request(:get, %r{www.europeana.eu/api/v2/search.json})).
           to have_been_made.once
       end
     end
 
-    context "with query" do
+    context 'with query' do
       let(:params) { { query: 'test' } }
 
-      it "sends query" do
+      it 'sends query' do
         subject
-        expect(a_request(:get, %r{www.europeana.eu/api/v2/search.json?query=test})).
+        expect(a_request(:get, 'www.europeana.eu/api/v2/search.json').
+          with(query: hash_including({'query' => params[:query]}))).
           to have_been_made.once
       end
     end
