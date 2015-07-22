a35778296e4e1066eedf1f9185b270d860c342ca
Richard Doe <richard.doe@rwdit.net>
2015-07-22 10:42:20 +0100

    Wear coveralls.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,11 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in europeana.gemspec
 gemspec
+
+group :test do
+  gem 'coveralls'
+end
+
+group :test, :develop do
+  gem 'rubocop'
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,10 @@
+require 'coveralls'
 require 'europeana/api'
 require 'webmock/rspec'
 
 Dir["./spec/support/**/*.rb"].each { |f| require f }
+
+Coveralls.wear! unless Coveralls.will_run?.nil?
 
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
