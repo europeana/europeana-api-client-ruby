320bb38796b67d7fed451ae9f3016fc6b6265f32
Richard Doe <richard.doe@rwdit.net>
2015-10-14 17:10:09 +0100

    Re-factor hierarchy support to be lower-level

---
+++ lib/europeana/api/record/hierarchy/ancestor_self_siblings.rb
@@ -0,0 +1,12 @@
+module Europeana
+  module API
+    class Record
+      class Hierarchy
+        ##
+        # Retrieve record full(ish) hierarchy data over the Europeana API
+        class AncestorSelfSiblings < Base
+        end
+      end
+    end
+  end
+end
---
+++ lib/europeana/api/record/hierarchy/children.rb
@@ -0,0 +1,12 @@
+module Europeana
+  module API
+    class Record
+      class Hierarchy
+        ##
+        # Retrieve record children hierarchy data over the Europeana API
+        class Children < Base
+        end
+      end
+    end
+  end
+end
