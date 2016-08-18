5ebe2fcb976bc283d417c0ec86d0ed13335c1949
Richard Doe <richard.doe@rwdit.net>
2016-08-18 18:03:47 +0100

    Stub spec for authenticated request

---
+++ spec/europeana/api/faraday_middleware/authenticated_request_spec.rb
@@ -0,0 +1,21 @@
+RSpec.describe Europeana::API::FaradayMiddleware::AuthenticatedRequest do
+  context 'with `Europeana::API.api_key` set' do
+    context 'without `wskey` param' do
+      it 'uses `Europeana::API.api_key`'
+    end
+
+    context 'with `wskey` param' do
+      it 'uses `wskey` param'
+    end
+  end
+
+  context 'without `Europeana::API.api_key` set' do
+    context 'without `wskey` param' do
+      it 'fails'
+    end
+
+    context 'with `wskey` param' do
+      it 'uses `wskey` param'
+    end
+  end
+end
