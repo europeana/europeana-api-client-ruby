4789407ee068804ce0b4aa440ad252b836a28aff
Richard Doe <richard.doe@rwdit.net>
2016-11-14 11:14:21 +0000

    Remove unused code

--- lib/europeana/api.rb
+++ lib/europeana/api.rb
@@ -19,7 +19,6 @@ module Europeana
     autoload :Annotation, 'europeana/api/annotation'
     autoload :FaradayMiddleware, 'europeana/api/faraday_middleware'
     autoload :Client, 'europeana/api/client'
-    autoload :Endpoint, 'europeana/api/endpoint'
     autoload :Errors, 'europeana/api/errors'
     autoload :Record, 'europeana/api/record'
     autoload :Request, 'europeana/api/request'
@@ -38,36 +37,13 @@ module Europeana
       # @return [String]
       attr_accessor :url
 
-      ##
-      # The maximum number of retries permitted
-      #
-      # Retries occur when a network request to the API fails. The default is 5
-      # retries before giving up.
-      #
-      # @return [Integer]
-      attr_accessor :max_retries
-
-      ##
-      # The number of seconds to wait between retries
-      #
-      # The default is 10 seconds.
-      #
-      # @return [Integer]
-      attr_accessor :retry_delay
-
       # @return [Logger]
       attr_writer :logger
-
-      attr_accessor :cache_store
-
-      attr_accessor :cache_expires_in
 
       ##
       # Sets configuration values to their defaults
       def defaults!
         self.url = 'https://www.europeana.eu/api'
-        self.max_retries = 5
-        self.retry_delay = 3
       end
 
       def logger
