0fadf0f2911fac026ccafd7aeae5491b68635f15
Richard Doe <richard.doe@rwdit.net>
2016-11-16 10:00:38 +0000

    Change "-" to "_" in JSON responses

--- lib/europeana/api/faraday_middleware/response/parse_json_to_open_struct.rb
+++ lib/europeana/api/faraday_middleware/response/parse_json_to_open_struct.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require 'faraday_middleware/response_middleware'
+require 'active_support/json'
 
 module Europeana
   module API
@@ -9,16 +10,30 @@ module Europeana
       #
       # Returns each object not as a `Hash` but as an `OpenStruct`.
       #
-      # @todo Deal with cases where Europeana APIs return non-JSON responses (when
-      #   they should not), and detects request failures from flags and messages
-      #   in JSON responses.
+      # Changes "-" in JSON field keys to "_", so that they become methods
+      # on the `OpenStruct`.
       class ParseJsonToOpenStruct < ::FaradayMiddleware::ResponseMiddleware
         dependency do
           require 'json' unless defined?(JSON)
         end
 
         define_parser do |body|
-          JSON.parse(body, object_class: OpenStruct) unless body.strip.empty?
+          unless body.strip.empty?
+            hash = JSON.parse(body)
+            underscored_hash = underscore_hash_keys(hash)
+            underscored_body = underscored_hash.to_json
+            JSON.parse(underscored_body, object_class: OpenStruct)
+          end
+        end
+
+        class << self
+          def underscore_hash_keys(hash)
+            hash.keys.each do |k|
+              hash[k] = underscore_hash_keys(hash[k]) if hash[k].is_a?(Hash)
+              hash[k.underscore] = hash.delete(k) if k =~ /-/
+            end
+            hash
+          end
         end
       end
     end
