5190ce7713e6258df3b9222058228db6bce9d170
Richard Doe <richard.doe@rwdit.net>
2017-02-21 11:24:56 +0000

    Hound

--- spec/europeana/api/response_spec.rb
+++ spec/europeana/api/response_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 RSpec.describe Europeana::API::Response do
   it 'handles blank responses' do
-    request = instance_double("Europeana::API::Request", endpoint: {})
-    faraday_response = instance_double("Faraday::Response", status: 204, body: '')
+    request = instance_double('Europeana::API::Request', endpoint: {})
+    faraday_response = instance_double('Faraday::Response', status: 204, body: '')
     response = described_class.new(request, faraday_response)
     expect { response.validate! }.not_to raise_error
     expect(response.body).to eq('')
