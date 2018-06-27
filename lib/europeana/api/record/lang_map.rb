ec5a93a9ed4fec0bfbc8e759e0d214c8e73643b3
Richard Doe <richard.doe@rwdit.net>
2018-06-27 15:06:15 +0100

    Documentation and specs for LangMap module

--- lib/europeana/api/record/lang_map.rb
+++ lib/europeana/api/record/lang_map.rb
@@ -19,11 +19,23 @@ module Europeana
         NON_ISO_LANG_CODES = ['def', '']
 
         class << self
-          def lang_map?(obj)
-            return false unless obj.is_a?(Hash)
-            obj.keys.map(&:to_s).all? { |key| known_lang_map_key?(key) }
+          # Is the argument a language map?
+          #
+          # Critera:
+          # * Must be a +Hash+.
+          # * Must have only keys which are known language codes
+          #
+          # @param candidate [Object] Object to check
+          # @return [Boolean]
+          def lang_map?(candidate)
+            return false unless candidate.is_a?(Hash)
+            candidate.keys.map(&:to_s).all? { |key| known_lang_map_key?(key) }
           end
 
+          # Is the argument a known language map key?
+          #
+          # @param key [String] String to check
+          # @return [Boolean]
           def known_lang_map_key?(key)
             key = key.dup.downcase
             DEPRECATED_ISO_LANG_CODES.include?(key) ||
@@ -31,9 +43,22 @@ module Europeana
               !ISO_639.find(key.split('-').first).nil?
           end
 
-          def localize_lang_map(lang_map)
+          # Localise a language map
+          #
+          # 1. If the argument is not a language map, return as-is
+          # 2. If the language map has a key for the current locale, return that
+          #    value
+          # 3. If the language map has a key for the default locale, return that
+          #    value
+          # 4. Else, return all values
+          #
+          # Arrays will be recursed over, localising each element.
+          #
+          # @param lang_map [Object] Object to attempt to localise
+          # @return [Array<Object>] Localised value
+          def localise_lang_map(lang_map)
             if lang_map.is_a?(Array)
-              return lang_map.map { |l| localize_lang_map(l) }
+              return lang_map.map { |val| localise_lang_map(val) }
             end
 
             return lang_map unless lang_map?(lang_map)
@@ -43,27 +68,16 @@ module Europeana
               lang_map.values
           end
 
+          # Fetch the language map value for a given locale
+          #
+          # @param lang_map [Hash] Verified language map
+          # @param locale [String] Locale to fetch the value for
+          # @return [Array<Object>,nil] Array of value(s) for the given locale,
+          #   or nil if none are present
           def lang_map_value(lang_map, locale)
             keys = salient_lang_map_keys(lang_map, locale)
             return nil unless keys.present?
             keys.map { |k| lang_map[k] }.flatten.uniq
-          end
-
-          def dereferenced_lang_map_value(value)
-            return nil if value.nil?
-
-            if value.is_a?(Array)
-              return value.map { |v| dereferenced_lang_map_value(v) }
-            end
-
-            return value unless value.is_a?(String)
-
-            concept = root.fetch('concepts', []).detect { |c| c[:about] == value }
-            if concept.present? && concept.key?(:prefLabel)
-              localize_lang_map(concept[:prefLabel])
-            else
-              return value
-            end
           end
 
           protected
